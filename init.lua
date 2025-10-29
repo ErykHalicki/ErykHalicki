22c8775e5dc2310c9b0dc962001b38ba7d687a78
Eryk Halicki <35389308+ErykHalicki@users.noreply.github.com>
2025-10-28 21:02:44 -0700

    Implement vim-plug auto-installation logic
    Add auto-installation of vim-plug if not present

--- init.lua
+++ init.lua
@@ -222,3 +222,10 @@ vim.api.nvim_create_user_command('Nuke', function()
   vim.cmd('NERDTreeClose')
   vim.cmd('xa')
 end, {})
+
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+if empty(glob(data_dir . '/autoload/plug.vim'))
+  silent execute '!curl -fLo '.data_dir.'/autoload/plug.vim --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+  autocmd VimEnter * PlugInstall --sync | source $MYVIMRC
+endif
+  
